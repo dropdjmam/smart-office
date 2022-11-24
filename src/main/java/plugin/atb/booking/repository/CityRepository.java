5ebf865ca253ea17e905549fd9e7d43a1f170112
Сергей Писарев <dropdjmam@mail.ru>
2022-11-24 11:43:32 +0000

    #28 Service City
    See merge request plugin-fall/team5/atb_plugin_backend!28

--- src/main/java/plugin/atb/booking/repository/CityRepository.java
+++ src/main/java/plugin/atb/booking/repository/CityRepository.java
@@ -1,10 +1,16 @@
 package plugin.atb.booking.repository;
 
+import java.util.*;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import plugin.atb.booking.entity.CityEntity;
 
 @Repository
 public interface CityRepository extends JpaRepository<CityEntity, Long> {
+
+    List<CityEntity> findAllByNameContainingOrderByName(String name);
+
+    boolean existsByName(String name);
 
 }
---
+++ src/main/java/plugin/atb/booking/service/CityService.java
@@ -0,0 +1,72 @@
+package plugin.atb.booking.service;
+
+import java.util.*;
+
+import lombok.*;
+import org.springframework.data.domain.*;
+import org.springframework.stereotype.*;
+import plugin.atb.booking.entity.*;
+import plugin.atb.booking.exception.*;
+import plugin.atb.booking.repository.*;
+
+@Service
+@RequiredArgsConstructor
+public class CityService {
+
+    private final CityRepository cityRepository;
+
+    public void add(String name) {
+        boolean exists = cityRepository.existsByName(name);
+
+        if (exists) {
+            throw new AlreadyExistsException(
+                "Город " + name + "уже существует!");
+        }
+
+        CityEntity city = new CityEntity();
+        city.setName(name);
+        cityRepository.save(city);
+    }
+
+    public List<CityEntity> getAll() {
+        return cityRepository.findAll(Sort.by(Sort.Direction.ASC, "name"));
+    }
+
+    public CityEntity getById(Long id) {
+        return cityRepository.findById(id).orElse(null);
+    }
+
+    public List<CityEntity> getAllByName(String name) {
+        return cityRepository.findAllByNameContainingOrderByName(name);
+    }
+
+    public void update(CityEntity city) {
+        String newName = city.getName();
+
+        boolean exists = cityRepository.existsByName(newName);
+
+        if (exists) {
+            throw new AlreadyExistsException("Город  " + newName + " уже существует!");
+        }
+
+        CityEntity cityUpdate = getById(city.getId());
+
+        if (cityUpdate == null) {
+            throw new NotFoundException("Город  с id " + cityUpdate.getId() + " не найден!");
+        }
+
+        cityUpdate.setName(newName);
+
+        cityRepository.save(cityUpdate);
+    }
+
+    public void delete(Long id) {
+
+        if (getById(id) == null) {
+            throw new NotFoundException("Города с id " + id + " не существует");
+        }
+
+        cityRepository.deleteById(id);
+    }
+
+}
