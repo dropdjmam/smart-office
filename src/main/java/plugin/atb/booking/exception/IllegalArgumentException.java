cf46f1b4abb2a857158cce3d7038890e6f13a9e2
Eudzen <eudzen@vk.com>
2022-11-25 06:20:44 +1000

    #40 Добавлены новое исключение, ControllerHandler

---
+++ src/main/java/plugin/atb/booking/exception/IllegalArgumentException.java
@@ -0,0 +1,9 @@
+package plugin.atb.booking.exception;
+
+public class IllegalArgumentException extends RuntimeException {
+
+    public IllegalArgumentException(String message) {
+        super(message);
+    }
+
+}
