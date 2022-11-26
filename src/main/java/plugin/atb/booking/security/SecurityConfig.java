b594780a31308aeb3e56cac16ae483ef901886ce
Евгений Орлов <eudzen@vk.com>
2022-11-26 15:48:51 +0000

    #27 Authentication
    See merge request plugin-fall/team5/atb_plugin_backend!45

---
+++ src/main/java/plugin/atb/booking/security/SecurityConfig.java
@@ -0,0 +1,70 @@
+package plugin.atb.booking.security;
+
+import javax.sql.*;
+
+import lombok.*;
+import org.springframework.context.annotation.*;
+import org.springframework.security.authentication.*;
+import org.springframework.security.config.annotation.authentication.configuration.*;
+import org.springframework.security.config.annotation.web.builders.*;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.crypto.bcrypt.*;
+import org.springframework.security.crypto.password.*;
+import org.springframework.security.provisioning.*;
+import org.springframework.security.web.*;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final DataSource dataSource;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+            .authorizeRequests()
+            .anyRequest().authenticated()
+
+            .and()
+            .logout()
+            .logoutUrl("/logout")
+            .invalidateHttpSession(true)
+            .clearAuthentication(true)
+            .deleteCookies("JSESSIONID")
+            .logoutSuccessUrl("/login")
+
+            .and()
+            .httpBasic();
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsManager users() {
+
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+
+        users.setUsersByUsernameQuery("Select login, password, 1 from employees "
+                                      + "where login = ?");
+
+        users.setAuthoritiesByUsernameQuery("Select e.login, r.name from employees as e "
+                                            + "inner join roles as r on (e.role_id=r.id) "
+                                            + "where e.login = ?");
+        return users;
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+        AuthenticationConfiguration authenticationConfiguration
+    ) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+}
---
+++ src/main/resources/db/migration/V0_1_19__insert_roles.sql
@@ -0,0 +1,4 @@
+INSERT INTO roles(name)
+VALUES ('ROLE_EMPLOYEE'),
+       ('ROLE_ADMIN'),
+       ('ROLE_TEAM_LEAD');
