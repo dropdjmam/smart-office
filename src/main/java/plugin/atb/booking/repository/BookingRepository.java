ef7bd5815bb39cebfce64e9b1739ebc004f1fb06
Евгений Орлов <eudzen@vk.com>
2022-11-27 12:31:54 +0000

    #8 Service Booking
    See merge request plugin-fall/team5/atb_plugin_backend!36

--- src/main/java/plugin/atb/booking/repository/BookingRepository.java
+++ src/main/java/plugin/atb/booking/repository/BookingRepository.java
@@ -1,10 +1,62 @@
 package plugin.atb.booking.repository;
 
+import java.time.*;
+import java.util.*;
+
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.*;
 import org.springframework.stereotype.*;
 import plugin.atb.booking.entity.*;
 
 @Repository
 public interface BookingRepository extends JpaRepository<BookingEntity, Long> {
+
+    /**
+     * Возвращает отсортированный список сущностей (бронирований),
+     * для которого выполняются следующие условия:
+     * <ul>
+     * <li> Рабочее место соответствует искомому (параметру workplace)
+     * <li> Начало полученной брони раньше конца брони из БД (т.е. start < dateTimeOfEnd)
+     * <li> Конец полученной брони позже начала брони из БД (т.е. end > dateTimeOfStart)
+     * <li> Данные сортируются по убыванию по дате/времени начала брони.
+     * </ul>
+     *
+     * @param workPlace бронируемое рабочее место
+     * @param startOfNewBooking дата/время начала новой брони, end
+     * @param endOfNewBooking дата/время окончания новой брони, start
+     *
+     * @return список бронирований
+     */
+    @Query(value = "select booking from BookingEntity booking " +
+                   "where booking.workPlace = :workPlace and " +
+                   "(booking.dateTimeOfStart < :end and " +
+                   "booking.dateTimeOfEnd > :start) " +
+                   "order by booking.dateTimeOfStart desc")
+    List<BookingEntity> findAllInPeriod(
+        @Param("workPlace") WorkPlaceEntity workPlace,
+        @Param("start") LocalDateTime startOfNewBooking,
+        @Param("end") LocalDateTime endOfNewBooking
+
+    );
+
+    /**
+     * Возвращает отсортированный список сущностей (актуальных бронирований),
+     * для которого выполняются следующие условия:
+     * <ul>
+     * <li> Возвращаются брони согласно полученному идентификатору пользователя
+     * (он же держатель брони/holder).
+     * <li> Время окончания брони из БД (dateTimeOfEnd) должно быть позже данного момента.
+     * <li> Данные сортируются по убыванию по дате/времени начала брони.
+     * </ul>
+     *
+     * @param id идентификатор пользователя, holder_id
+     *
+     * @return список бронирований
+     */
+    @Query("select booking from BookingEntity booking " +
+           "where booking.holder.id = :holder_id and " +
+           "booking.dateTimeOfEnd > current_timestamp " +
+           "order by booking.dateTimeOfStart desc")
+    List<BookingEntity> findAllActual(@Param("holder_id") Long id);
 
 }
---
+++ src/main/java/plugin/atb/booking/service/BookingService.java
@@ -0,0 +1,125 @@
+package plugin.atb.booking.service;
+
+import java.time.*;
+import java.util.*;
+
+import lombok.*;
+import org.springframework.data.domain.*;
+import org.springframework.stereotype.*;
+import plugin.atb.booking.entity.*;
+import plugin.atb.booking.exception.*;
+import plugin.atb.booking.repository.*;
+
+@Service
+@RequiredArgsConstructor
+public class BookingService {
+
+    private static final int PAGE_LIMIT = 8;
+
+    private final BookingRepository bookingRepository;
+
+    public void add(BookingEntity booking) {
+
+        if (booking.getWorkPlace() == null) {
+            throw new NotFoundException("Место для брони не найдено");
+        }
+
+        LocalDateTime start = booking.getDateTimeOfStart();
+        LocalDateTime end = booking.getDateTimeOfEnd();
+
+        boolean validTimeInterval = end.isAfter(start);
+
+        if (!validTimeInterval) {
+            throw new IllegalArgumentException(String.format(
+                "Конец брони не может быть раньше начала: %s < $s",
+                end, start
+            ));
+        }
+
+        boolean bookingsInTime = bookingRepository
+            .findAllInPeriod(booking.getWorkPlace(), start, end).isEmpty();
+
+        if (!bookingsInTime) {
+            throw new AlreadyExistsException(String.format(
+                "Невозможно забронировать данное место на данное время: %s - $s",
+                start, end
+            ));
+        }
+
+        if (booking.getGuests() < 0) {
+            throw new IllegalArgumentException("Гостей не может быть меньше нуля");
+        }
+
+        bookingRepository.save(booking);
+
+    }
+
+    public List<BookingEntity> getAllInPeriod(
+        WorkPlaceEntity workPlace,
+        LocalDateTime start,
+        LocalDateTime end
+    ) {
+        return bookingRepository.findAllInPeriod(workPlace, start, end);
+    }
+
+    public List<BookingEntity> getAllActual(Long id) {
+        return bookingRepository.findAllActual(id);
+    }
+
+    public List<BookingEntity> getPage(Integer pageNumber) {
+        Sort sort = Sort.by(Sort.Direction.DESC, "dateTimeOfStart");
+
+        PageRequest request = PageRequest.of(pageNumber, PAGE_LIMIT, sort);
+
+        Page<BookingEntity> page = bookingRepository.findAll(request);
+
+        if (page.isEmpty()) {
+            return new ArrayList<>();
+        }
+        return page.getContent();
+    }
+
+    public BookingEntity getById(Long id) {
+        return bookingRepository.findById(id).orElse(null);
+    }
+
+    public void update(BookingEntity booking) {
+        BookingEntity updateBooking = getById(booking.getId());
+
+        if (updateBooking == null) {
+            throw new NotFoundException("Бронирование не найдено");
+        }
+
+        if (booking.getWorkPlace() != null) {
+            updateBooking.setWorkPlace(booking.getWorkPlace());
+        }
+
+        if (booking.getMaker() != null) {
+            updateBooking.setMaker(booking.getMaker());
+        }
+
+        if (booking.getDateTimeOfStart() != null) {
+            updateBooking.setDateTimeOfStart(booking.getDateTimeOfStart());
+        }
+
+        if (booking.getDateTimeOfEnd() != null) {
+            updateBooking.setDateTimeOfEnd(booking.getDateTimeOfEnd());
+        }
+
+        if (booking.getGuests() > 0) {
+            updateBooking.setGuests(booking.getGuests());
+        }
+
+        bookingRepository.save(updateBooking);
+    }
+
+    public void delete(Long id) {
+
+        if (getById(id) == null) {
+            throw new NotFoundException("Не найдено бронирование с id: " + id);
+        }
+
+        bookingRepository.deleteById(id);
+    }
+
+}
